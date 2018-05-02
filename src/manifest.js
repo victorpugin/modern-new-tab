a105e1b942da29727a3342832ed2c5543bc33d20
victor pugin <victor.pugin@epitech.eu>
2018-05-02 20:53:01 +0200

    0.2.5

--- src/manifest.js
+++ src/manifest.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'Modern New Tab',
   description: 'A modern and clean new tab extension',
   author: 'victor pugin <victor.pugin@epitech.eu>',
-  version: '0.2.4',
+  version: '0.2.5',
   icons: {
     '128': 'icons/icon_128.png'
   },
