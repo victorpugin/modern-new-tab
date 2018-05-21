434d35d11f8769a22521c57697f4ebc994677ca3
victorpugin <victor.pugin@epitech.eu>
2018-05-21 13:57:48 +0200

    fix lint issues

--- src/manifest.js
+++ src/manifest.js
@@ -13,15 +13,6 @@ module.exports = {
    * @see {@link https://developer.chrome.com/extensions/declare_permissions}
    */
   permissions: [
-// Temporary disabled unused permissions
-//    '<all_urls>',
-//    '*://*/*',
-/*
-    'activeTab',
-    'tabs',
-    'background',
-    'unlimitedStorage',
-*/
     'storage',
     'bookmarks',
     'chrome://favicon/',
@@ -35,24 +26,12 @@ module.exports = {
     persistent: false,
     page: 'pages/background.html'
   },
-// Pages for future features
-/*  options_page: 'pages/options.html',
-  content_scripts: [{
-    js: [
-      'js/manifest.js',
-      'js/vendor.js',
-      'js/content.js'
-    ],
-    run_at: 'document_end',
-    matches: ['<all_urls>'],
-    all_frames: true
-  }],*/
   manifest_version: 2,
   content_security_policy: "script-src 'self'; object-src 'self'",
   web_accessible_resources: [
     'js/content.js'
   ],
   chrome_url_overrides: {
-    "newtab": "pages/app.html"
+    'newtab': 'pages/app.html'
   }
 }
